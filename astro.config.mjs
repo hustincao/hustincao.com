271184d2a7217516988ff5e456650ea7ea8fb382
Hustin Cao <hustincao@gmail.com>
2025-08-30 01:10:52 -0400

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hustincao.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
